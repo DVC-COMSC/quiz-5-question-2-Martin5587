a1a0c15215722e8612d34d461d226767dd717041
Martin5587 <mcontreras265@insite.4cd.edu>
2024-03-07 20:58:41 -0800

    commit(1)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,20 @@ int main()
    
    cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
    cin >> N >> M;
-   /* Complete your code */
+  
+  if (N <= 0 || N >= 10 || M < 1 || M > 10) {
+   cout << "ERROR. Valid range is between 0 < N < 10 and 1 <= M <= 10." << endl;
+   return 1;
+  }
+
+  cout << "Output: ";
+  int result = 1;
+  for (int i = 0; i <= M; ==i) {
+   cout << result << " ";
+   result *= N;
+  }
+  cout << endl;
+
+  return 0;
   
 }
