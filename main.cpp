cc6e2523a9572f98b6fa10ffedadd267883c9fb9
Martin5587 <mcontreras265@insite.4cd.edu>
2024-03-07 21:10:11 -0800

    commit(input/output changes were made for a better experience)

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@ int main()
 {
    int N, M;
    
-   cout << "Enter your N, RULES: M(0 < N < 10,  1 <= M <= 10): ";
-   cin >> N >> M;
+   cout << "Enter the base number: ";
+   cin >> N;
   
-  if (N <= 0 || N >= 10 || M < 1 || M > 10) {
+  if (N <= 0 || N >= 10) {
    cout << "ERROR. Valid range is between 0 < N < 10 and 1 <= M <= 10." << endl;
+   return 1;
+  }
+
+  cout << "Enter the exponent: ";
+  cin >> M;
+
+  if (M < 1 || M > 10) {
+   cout << "ERROR. Valid range for M is between 1 <= M <= 10." << endl;
    return 1;
   }
 
